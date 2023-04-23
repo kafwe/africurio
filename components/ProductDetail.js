2b838e6a7867c081c28498b3c3a78e4cc2a7b94c
Jordy Kafwe <jordykafwe@gmail.com>
2023-04-23 21:19:10 +0200

    Add Product Grid

--- components/ProductDetail.js
+++ components/ProductDetail.js
@@ -1,18 +1,17 @@
-import Link from 'next/link';
 import QuantityCounter from './QuantityCounter';
 
-export default function ProductDetail() {
+export default function ProductDetail({ product }) {
   return (
         <section className="text-gray-600 body-font overflow-hidden">
         <div className="container px-5 py-24 mx-auto">
             <div className="lg:w-4/5 mx-auto flex flex-wrap">
-            <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400"/>
+            <img className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src={product.imageSrc}/>
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                <h2 className="text-sm title-font text-gray-500 tracking-widest">DECOR</h2>
-                <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
+                <h2 className="text-sm title-font text-gray-500 tracking-widest">{product.categoryName.toUpperCase()}</h2>
+                <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.name}</h1>
                 <div className="flex mb-4">
                 </div>
-                <p className="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+                <p className="leading-relaxed">{product.description}</p>
                 <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                     <div className="flex items-center">
                         <span className="mr-3">Quantity</span>
@@ -26,7 +25,7 @@ export default function ProductDetail() {
                     </div>
                 </div>
                 <div className="flex">
-                <span className="title-font font-medium text-2xl text-gray-900">R58</span>
+                <span className="title-font font-medium text-2xl text-gray-900">R{product.price}</span>
                 <button className="flex ml-auto text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded">Add to cart</button>
                 </div>
             </div>
---
+++ components/ProductGrid.js
@@ -0,0 +1,23 @@
+import ProductCard from './ProductCard';
+
+export default function ProductGrid({ category }) {
+    const filteredProducts = category ? 
+    products.filter(product => product.category === category) : products;
+
+    const categoryHeading = category ? 
+    filteredProducts[0].categoryName : 'Shop All';
+
+    return (
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 className="">{categoryHeading}</h2>
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {filteredProducts.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+}
+  
