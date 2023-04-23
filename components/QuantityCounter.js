d95f93b2fc400b1ccb67feffb3090384dc70b63b
Jordy Kafwe <jordykafwe@gmail.com>
2023-04-23 20:51:09 +0200

    Add Quantity Number Input Counter

---
+++ components/QuantityCounter.js
@@ -0,0 +1,28 @@
+import {
+    Button,
+    useNumberInput,
+    HStack,
+    Input
+  } from '@chakra-ui/react';
+
+
+export default function QuantityCounter({ defaultValue, size }) {
+    const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
+    useNumberInput({
+      step: 1,
+      defaultValue: defaultValue || 1,
+      min: 1
+    })
+
+    const inc = getIncrementButtonProps()
+    const dec = getDecrementButtonProps()
+    const input = getInputProps()
+
+    return (
+        <HStack maxW='320px'>
+            <Button {...inc}>+</Button>
+            <Input {...input} />
+            <Button {...dec}>-</Button>
+        </HStack>
+    )
+}
