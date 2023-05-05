1dc95f0bfc3fd3ce1775be968c1da8d34f0c365d
Jordy Kafwe <jordykafwe@gmail.com>
2023-05-05 19:24:01 +0200

    Change product image

--- components/ProductDetail.js
+++ components/ProductDetail.js
@@ -6,8 +6,6 @@ import {
 } from '@chakra-ui/react';
 import { useState } from 'react'
 
-
-
 export default function ProductDetail({ product }) {
     const [quantity, setQuantity] = useState(1)
     const handleChange = (value) => setQuantity(value)
@@ -16,6 +14,7 @@ export default function ProductDetail({ product }) {
         const cartItem = { ...product, quantity }
         localStorage.setItem(product.id, JSON.stringify(cartItem))
     }
+
     const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
     useNumberInput({
       step: 1,
@@ -27,41 +26,42 @@ export default function ProductDetail({ product }) {
     const dec = getDecrementButtonProps()
     const input = getInputProps()
 
+    if (!product) return null;
 
-  return (
-        <section className="text-gray-600 body-font overflow-hidden">
-        <div className="container px-5 py-24 mx-auto">
-            <div className="lg:w-4/5 mx-auto flex flex-wrap">
-            <img className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" alt={''} src={`/${product.imageSrc}`}/>
-            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                <h2 className="text-sm title-font text-gray-500 tracking-widest">{product.categoryName.toUpperCase()}</h2>
-                <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.name}</h1>
-                <div className="flex mb-4">
-                </div>
-                <p className="leading-relaxed">{product.description}</p>
-                <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-                    <div className="flex items-center">
-                        <span className="mr-3">Quantity</span>  
-                        <div className="relative">
-                            <div className='pl-3 pr-10'>
-                                <HStack maxW='320px'>
-                                    <Button {...inc}>+</Button>
-                                    <Input {...input} onChange={handleChange} />
-                                    <Button {...dec}>-</Button>
-                                </HStack>
-                            </div>
-                           
+    return (
+            <section className="text-gray-600 body-font overflow-hidden">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="lg:w-4/5 mx-auto flex flex-wrap">
+                <img className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" alt={product.imageAlt} src={`/${product.imageSrc}`}/>
+                <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                    <h2 className="text-sm title-font text-gray-500 tracking-widest">{product.categoryName.toUpperCase()}</h2>
+                    <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.name}</h1>
+                    <div className="flex mb-4">
+                    </div>
+                    <p className="leading-relaxed">{product.description}</p>
+                    <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
+                        <div className="flex items-center">
+                            <span className="mr-3">Quantity</span>  
+                            <div className="relative">
+                                <div className='pl-3 pr-10'>
+                                    <HStack maxW='320px'>
+                                        <Button {...inc}>+</Button>
+                                        <Input {...input} onChange={handleChange} />
+                                        <Button {...dec}>-</Button>
+                                    </HStack>
+                                </div>
                             
+                                
+                            </div>
                         </div>
                     </div>
+                    <div className="flex">
+                    <span className="title-font font-medium text-2xl text-gray-900">R{product.price}</span>
+                    <button className="flex ml-auto text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded" onClick={handleClick}>Add to cart</button>
+                    </div>
                 </div>
-                <div className="flex">
-                <span className="title-font font-medium text-2xl text-gray-900">R{product.price}</span>
-                <button className="flex ml-auto text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded" onClick={handleClick}>Add to cart</button>
                 </div>
             </div>
-            </div>
-        </div>
-        </section>
-  );
+            </section>
+    );
 }
--- data/products.js
+++ data/products.js
@@ -5,7 +5,7 @@ const products = [
           "description": "This beautiful hand-carved wooden bowl is perfect for displaying fruit or other decorative items. The intricate designs and patterns make it a true work of art.",
           "category": "decor",
           "categoryName": "Decor",
-          "price": 80,
+          "price": 800,
           "imageSrc": "bowl.jpg",
           "imageAlt": "Hand-Carved Wooden Bowl"
         },
@@ -15,7 +15,7 @@ const products = [
           "description": "Add a pop of color and culture to your dining table with this African print table runner. Made with high-quality fabric and featuring bold patterns and colors, this table runner is sure to impress.",
           "category": "decor",
           "categoryName": "Decor",
-          "price": 25,
+          "price": 250,
           "imageSrc": "tablerunner.jpg",
           "imageAlt": "African Print Table Runner"
         },
@@ -25,7 +25,7 @@ const products = [
           "description": "This hand-woven basket is perfect for storing blankets, magazines, or other household items. Made with natural fibers and featuring intricate patterns, this basket is both beautiful and functional.",
           "category": "decor",
           "categoryName": "Decor",
-          "price": 45,
+          "price": 450,
           "imageSrc": "basket.png",
           "imageAlt": "Hand-Woven Basket"
         },
@@ -35,7 +35,7 @@ const products = [
           "description": "This stunning African mask is made from high-quality materials and features intricate carvings and patterns. Hang it on your wall to add a touch of African culture and art to your home.",
           "category": "decor",
           "categoryName": "Decor",
-          "price": 120,
+          "price": 1200,
           "imageSrc": "mask.webp",
           "imageAlt": "African Mask Wall Art"
         },
@@ -45,7 +45,7 @@ const products = [
           "description": "This beautiful hand-painted ceramic vase is perfect for displaying fresh flowers or other decorative items. The intricate designs and patterns make it a true work of art.",
           "category": "decor",
           "categoryName": "Decor",
-          "price": 65,
+          "price": 650,
           "imageSrc": "vase.jpg",
           "imageAlt": "Hand-Painted Ceramic Vase"
         },
@@ -55,7 +55,7 @@ const products = [
           "description": "This beautiful African tribal wall hanging is made from high-quality materials and features intricate patterns and designs. Hang it on your wall to add a touch of African culture and art to your home.",
           "category": "decor",
           "categoryName": "Decor",
-          "price": 90,
+          "price": 900,
           "imageSrc": "wall-hanging.jpg",
           "imageAlt": "African Tribal Wall Hanging"
         },
@@ -65,7 +65,7 @@ const products = [
             "description": "This beautiful malachite rhino statue is a true work of art. The rhino is carved in incredible detail.",
             "category": "decor",
             "categoryName": "Decor",
-            "price": 120,
+            "price": 1200,
             "imageSrc": "rhino.jpg",
             "imageAlt": "Rhino Statue"
         },
@@ -75,7 +75,7 @@ const products = [
             "description": "This beautiful Kente cloth scarf is handwoven in Ghana and features a traditional Kente design. The scarf is made from high-quality cotton and can be worn as a fashionable accessory or used to keep you warm on chilly days. It also makes for a great gift for friends and family.",
             "category": "clothing",
             "categoryName": "Clothing & Accessories",
-            "price": 29.99,
+            "price": 290,
             "imageSrc": "kente-cloth-scarf.jpg",
             "imageAlt": "Kente cloth scarf"
             },
@@ -85,7 +85,7 @@ const products = [
               "description": "This stunning Maasai beaded necklace is handcrafted in Kenya and features a traditional Maasai design. The necklace is made from colorful beads and can be worn as a statement piece to add a pop of color to any outfit. It is also a great way to show support for the Maasai community.",
               "category": "clothing",
               "categoryName": "Clothing & Accessories",
-              "price": 39.99,
+              "price": 390,
               "imageSrc": "maasai-beaded-necklace.webp",
               "imageAlt": "Maasai beaded necklace"
             },
@@ -95,7 +95,7 @@ const products = [
               "description": "This beautiful African print headwrap is handcrafted in Nigeria and features a vibrant African print design. The headwrap can be worn in a variety of styles to suit your mood and outfit, and is made from high-quality cotton fabric that is gentle on your skin.",
               "category": "clothing",
               "categoryName": "Clothing & Accessories",
-              "price": 19.99,
+              "price": 190,
               "imageSrc": "african-print-headwrap.webp",
               "imageAlt": "African print headwrap"
             },
@@ -105,7 +105,7 @@ const products = [
               "description": "These stylish beaded leather sandals are handcrafted in Morocco and feature intricate beadwork on the straps. The sandals are made from high-quality leather and are both comfortable and durable, making them perfect for everyday wear. They also make for a great gift for friends and family.",
               "category": "clothing",
               "categoryName": "Clothing & Accessories",
-              "price": 79.99,
+              "price": 790,
               "imageSrc": "beaded-leather-sandals.webp",
               "imageAlt": "Beaded leather sandals"
             },
@@ -115,7 +115,7 @@ const products = [
               "description": "This stylish Kente cloth bow tie is handwoven in Ghana and features a traditional Kente design. The bow tie is made from high-quality cotton and is perfect for adding a touch of African culture to any formal outfit. It also makes for a great gift for the stylish men in your life.",
               "category": "clothing",
               "categoryName": "Clothing & Accessories",
-              "price": 24.99,
+              "price": 240,
               "imageSrc": "kente-cloth-bow-tie.webp",
               "imageAlt": "Kente cloth bow tie"
             },
@@ -125,7 +125,7 @@ const products = [
               "description": "This authentic Zulu shield is made from cowhide and wood and decorated with traditional patterns. It is a symbol of the Zulu warriors and their bravery in battle. The shield can be used as a wall decoration or a cultural souvenir.",
               "category": "arts",
               "categoryName": "Arts & Collectibles",
-              "price": 49.99,
+              "price": 490,
               "imageSrc": "zulu-shield.webp",
               "imageAlt": "Zulu shield"
             },
@@ -135,8 +135,8 @@ const products = [
               "description": "This beautiful Shona stone sculpture is carved from serpentine stone by skilled artisans in Zimbabwe. It depicts a mother and child in a loving embrace and represents the bond between family. The sculpture is a unique piece of art that can enhance any space.",
               "category": "arts",
               "categoryName": "Arts & Collectibles",
-              "price": 59.99,
-              "imageSrc": "shona-stone-sculpture.webp",
+              "price": 590,
+              "imageSrc": "shona-stone-sculpture.jpg",
               "imageAlt": "Shona stone sculpture"
             },
             {
@@ -145,8 +145,8 @@ const products = [
               "description": "This colorful Tinga Tinga painting is inspired by the wildlife and nature of Tanzania. It is painted with enamel paint on canvas and features a vibrant scene of animals and plants. The painting is a perfect example of the Tinga Tinga style, which originated from the artist Edward Saidi Tingatinga.",
               "category": "arts",
               "categoryName": "Arts & Collectibles",
-              "price": 69.99,
-              "imageSrc": "tinga-tinga-painting.webp",
+              "price": 690,
+              "imageSrc": "tinga-tinga-painting.jpg",
               "imageAlt": "Tinga Tinga painting"
             },
             {
@@ -155,8 +155,8 @@ const products = [
               "description": "This stunning Bogolanfini mud cloth is handwoven and dyed with natural materials by the Bamana people of Mali. It features geometric patterns and symbols that have cultural and spiritual meanings. The cloth can be used as a wall hanging, a tablecloth, or a throw.",
               "category": "arts",
               "categoryName": "Arts & Collectibles",
-              "price": 79.99,
-              "imageSrc": "bogolanfini-mud-cloth.webp",
+              "price": 790,
+              "imageSrc": "bogolanfini-mud-cloth.jpg",
               "imageAlt": "Bogolanfini mud cloth"
             },
             {
@@ -165,8 +165,8 @@ const products = [
               "description": "This charming Ndebele doll is handmade by the Ndebele women of South Africa. It is dressed in colorful beads and fabric that reflect the Ndebele culture and identity. The doll can be used as a toy, a decoration, or a gift.",
               "category": "arts",
               "categoryName": "Arts & Collectibles",
-              "price": 29.99,
-              "imageSrc": "ndebele-doll.webp",
+              "price": 290,
+              "imageSrc": "ndebele-doll.jpg",
               "imageAlt": "Ndebele doll"
           },
           {
@@ -175,8 +175,8 @@ const products = [
               "description": "This elegant Kuba cloth pillow cover is made from raffia fiber that is dyed and embroidered by the Kuba people of Congo. It features intricate patterns and textures that showcase the Kuba artistry and craftsmanship. The pillow cover can add a touch of sophistication and comfort to any sofa or bed.",
               "category": "arts",
               "categoryName": "Arts & Collectibles",
-              "price": 19.99,
-              "imageSrc": "kuba-cloth-pillow-cover.webp",
+              "price": 190,
+              "imageSrc": "kuba-cloth-pillow-cover.jpg",
               "imageAlt": "Kuba cloth pillow cover"
           }
       
